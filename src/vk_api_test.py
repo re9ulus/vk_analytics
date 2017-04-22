bbd6ca4c198f1532a797416bb87d8f7da9446328
Vasiliy Zemlyanov <angeldaren@gmail.com>
2017-04-22 11:55:10 +0300

    Update vk_api_test.py

--- src/vk_api_test.py
+++ src/vk_api_test.py
@@ -1,53 +1,66 @@
 import requests
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 TOKEN_FILE = 'vk.token'
 
 
 class VkAPI(object):
 
-    def __init__(self, key, version='5.60'):
+    def __init__(self, token :str, version='5.60'):
         self._api_url = 'https://api.vk.com/method/'
-        self._key = key
+        self._token = token
         self._version = version
+        self._auth_params = ['{0}={1}'.format(key, val) for key, val in
+            {'access_token': self._token, 'v': self._version}.items()]
 
-    def wall_get(self, params):
+    def wall_get(self, params :dict, return_text=True):
         url = self._api_url + 'wall.get?'
-        param_string = ['{0}={1}'.format(key, val) for key, val in params.items()]
-        param_string += ['{0}={1}'.format(key, val) for key, val in
-            {'access_token': self._key, 'v': self._version}.items()]
-        url += '&'.join(param_string)
+        params = dict(params)
+        params['access_token'] = self._token
+        params['v'] = self._version
+        resp = None
         try:
-            resp = requests.get(url).text  # json()
-        except:
-            pass
-
+            resp = requests.get(url, params=params)
+            if return_text:
+                resp = resp.text
+            else:
+                resp = resp.json()
+        except Exception as ex:
+            logger.exception('{0}\nurl: {1}\nparams: {}'.format(ex, url, params))
         return resp
 
-
-def test_get(token):
-    vk = VkAPI(token)
-    params = {'domain': 'sevastopolsearch', 'count': 100, 'offset': 0}
-
-    delta_offset = 100
-
-    with open('test_res.txt', 'w+', encoding='utf-8') as f:
-        for i in range(10):
-            try:
-                print('request {0} offset {1}'.format(params['domain'], params['offset']))
-                resp = vk.wall_get(params)
-                print('response length: {}'.format(len(resp)))
-                f.write(resp)
-            except Exception as ex:
-                print('exception: {}'.format(ex))
+    def wall_get_all(self, domain :str, count=100, offset=0, delta_offset=100, pages_to_get=10):
+        params = {'domain': domain , 'count': count, 'offset': offset}
+        for i in range(pages_to_get):
+            logger.info('get page: {0}/{1}'.format(i+1, pages_to_get))
+            yield self.wall_get(params)
             params['offset'] += delta_offset
 
 
+def wall_to_file(filename, wall):
+    with open(filename, 'w+', encoding='utf-8') as f:
+        for data in wall:
+            if data:
+                try:
+                    f.write(data)
+                except Exception as ex:
+                    logger.exception('{0}'.format(ex))
 
-if __name__ == '__main__':
 
+def main_test():
+    logger.info('start')
     with open(TOKEN_FILE) as f:
         token = f.read().strip()
+    vk = VkAPI(token)
+    filename = 'test_res.txt'
+    domain = 'sevastopolsearch'
+    wall_to_file(filename, vk.wall_get_all(domain))
+    logger.info('done')
 
-    test_get(token)
-    print('done')
+
+if __name__ == '__main__':
+    main_test()
